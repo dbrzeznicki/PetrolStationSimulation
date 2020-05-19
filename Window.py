1e2c9d09b5e4a01923d54c2c49d10bddca2cdeec
dbrzeznicki <d.brzeznicki@wp.pl>
2020-05-19 11:40:07 +0200

    Set layout

--- Window.py
+++ Window.py
@@ -1,88 +1,107 @@
 import sys
-from PySide2.QtWidgets import QApplication, QPushButton, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit, QGroupBox
+
+from PySide2.QtWidgets import QApplication, QPushButton, QWidget, QGridLayout, QLineEdit, \
+    QTextEdit, QGroupBox, QFormLayout
 
 import PetrolStation
 from PetrolStation import start
 
 app = QApplication(sys.argv)
-label1 = QLabel("SIM_TIME")
-label2 = QLabel("GAS_STATION_SIZE")
-label3 = QLabel("THRESHOLD [%]")
-label4 = QLabel("NUMBER_OF_PUMP")
-label5 = QLabel("FUEL_TANK_SIZE")
-label6 = QLabel("REFUELING_SPEED")
-label7 = QLabel("TANK_TRUCK_TIME")
-label8 = QLabel("FUEL_TANK_LEVEL_MIN")
-label9 = QLabel("FUEL_TANK_LEVEL_MAX")
-label10 = QLabel("T_INTER_MIN")
-label11 = QLabel("T_INTER_MAX")
 
-textBox1 = QLineEdit("10000")
-textBox2 = QLineEdit("2000")
-textBox3 = QLineEdit("10")
-textBox4 = QLineEdit("2")
-textBox5 = QLineEdit("50")
-textBox6 = QLineEdit("2")
-textBox7 = QLineEdit("30")
-textBox8 = QLineEdit("5")
-textBox9 = QLineEdit("25")
-textBox10 = QLineEdit("10")
-textBox11 = QLineEdit("30")
+textBoxTableDiesel = [QLineEdit("10000"), QLineEdit("2000"), QLineEdit("10"), QLineEdit("2"), QLineEdit("50"),
+                      QLineEdit("2"), QLineEdit("30"), QLineEdit("5"), QLineEdit("25"), QLineEdit("10"),
+                      QLineEdit("30")]
 
-bigEditor = QTextEdit()
+ResultsDiesel = QTextEdit()
+Results9598 = QTextEdit()
+ResultsGas = QTextEdit()
+
+GroupBoxFormDiesel = QGroupBox("Diesel pump")
+GroupBoxForm9598 = QGroupBox("95/98")
+GroupBoxFormGas = QGroupBox("Gas")
+
+GroupBoxResultsDiesel = QGroupBox("Results diesel")
+GroupBoxResults9598 = QGroupBox("Results 95/98")
+GroupBoxResultsGas = QGroupBox("Results gas")
+
+calculateButton = QPushButton("Calculate")
 
 
-def say_hello():
-    PetrolStation.SIM_TIME = int(textBox1.text())
-    PetrolStation.GAS_STATION_SIZE = int(textBox2.text())
-    PetrolStation.THRESHOLD = int(textBox3.text())
-    PetrolStation.NUMBER_OF_PUMP = int(textBox4.text())
-    PetrolStation.FUEL_TANK_SIZE = int(textBox5.text())
-    PetrolStation.REFUELING_SPEED = int(textBox6.text())
-    PetrolStation.TANK_TRUCK_TIME = int(textBox7.text())
-    PetrolStation.FUEL_TANK_LEVEL = [int(textBox8.text()), int(textBox9.text())]
-    PetrolStation.T_INTER = [int(textBox10.text()), int(textBox11.text())]
-
+def set_diesel_properties():
+    PetrolStation.SIM_TIME = int(textBoxTableDiesel[0].text())
+    PetrolStation.GAS_STATION_SIZE = int(textBoxTableDiesel[1].text())
+    PetrolStation.THRESHOLD = int(textBoxTableDiesel[2].text())
+    PetrolStation.NUMBER_OF_PUMP = int(textBoxTableDiesel[3].text())
+    PetrolStation.FUEL_TANK_SIZE = int(textBoxTableDiesel[4].text())
+    PetrolStation.REFUELING_SPEED = int(textBoxTableDiesel[5].text())
+    PetrolStation.TANK_TRUCK_TIME = int(textBoxTableDiesel[6].text())
+    PetrolStation.FUEL_TANK_LEVEL = [int(textBoxTableDiesel[7].text()), int(textBoxTableDiesel[8].text())]
+    PetrolStation.T_INTER = [int(textBoxTableDiesel[9].text()), int(textBoxTableDiesel[10].text())]
     start()
-    bigEditor.append(PetrolStation.TEXT)
+    ResultsDiesel.append(PetrolStation.TEXT)
+
+
+def set_calculate_button():
+    calculateButton.clicked.connect(set_diesel_properties)
+
+
+def set_form_diesel():
+    formDiesel = QFormLayout()
+    formDiesel.addRow("SIM_TIME", textBoxTableDiesel[0])
+    formDiesel.addRow("GAS_STATION_SIZE", textBoxTableDiesel[1])
+    formDiesel.addRow("THRESHOLD [%]", textBoxTableDiesel[2])
+    formDiesel.addRow("NUMBER_OF_PUMP", textBoxTableDiesel[3])
+    formDiesel.addRow("FUEL_TANK_SIZE", textBoxTableDiesel[4])
+    formDiesel.addRow("TANK_TRUCK_TIME", textBoxTableDiesel[5])
+    formDiesel.addRow("TANK_TRUCK_TIME", textBoxTableDiesel[6])
+    formDiesel.addRow("FUEL_TANK_LEVEL_MIN", textBoxTableDiesel[7])
+    formDiesel.addRow("FUEL_TANK_LEVEL_MAX", textBoxTableDiesel[8])
+    formDiesel.addRow("T_INTER_MIN", textBoxTableDiesel[9])
+    formDiesel.addRow("T_INTER_MAX", textBoxTableDiesel[10])
+    GroupBoxFormDiesel.setLayout(formDiesel)
+
+
+def set_grid_result_diesel():
+    gridResultDiesel = QGridLayout()
+    gridResultDiesel.addWidget(ResultsDiesel, 0, 0)
+    GroupBoxResultsDiesel.setLayout(gridResultDiesel)
+
+
+def set_grid_result_9598():
+    gridResult9598 = QGridLayout()
+    gridResult9598.addWidget(Results9598, 0, 0)
+    GroupBoxResults9598.setLayout(gridResult9598)
+
+
+def set_grid_result_gas():
+    gridResultGas = QGridLayout()
+    gridResultGas.addWidget(ResultsGas, 0, 0)
+    GroupBoxResultsGas.setLayout(gridResultGas)
+
+
+def set_layout_window():
+    layoutWindow = QGridLayout()
+    layoutWindow.addWidget(GroupBoxFormDiesel, 0, 0)
+    layoutWindow.addWidget(GroupBoxForm9598, 0, 1)
+    layoutWindow.addWidget(GroupBoxFormGas, 0, 2)
+    layoutWindow.addWidget(calculateButton, 1, 0, 1, 3)
+    layoutWindow.addWidget(GroupBoxResultsDiesel, 2, 0)
+    layoutWindow.addWidget(GroupBoxResults9598, 2, 1)
+    layoutWindow.addWidget(GroupBoxResultsGas, 2, 2)
+    return layoutWindow
 
 
 class Window:
-
     window = QWidget()
+    set_calculate_button()
 
-    button1 = QPushButton("Calculate")
+    set_form_diesel()
 
-    button1.clicked.connect(say_hello)
+    set_grid_result_diesel()
+    set_grid_result_9598()
+    set_grid_result_gas()
 
-    layout = QGridLayout()
-    layout.addWidget(label1, 0, 0)
-    layout.addWidget(textBox1, 0, 1)
-    layout.addWidget(label2, 1, 0)
-    layout.addWidget(textBox2, 1, 1)
-    layout.addWidget(label3, 2, 0)
-    layout.addWidget(textBox3, 2, 1)
-    layout.addWidget(label4, 3, 0)
-    layout.addWidget(textBox4, 3, 1)
-    layout.addWidget(label5, 4, 0)
-    layout.addWidget(textBox5, 4, 1)
-    layout.addWidget(label6, 5, 0)
-    layout.addWidget(textBox6, 5, 1)
-    layout.addWidget(label7, 6, 0)
-    layout.addWidget(textBox7, 6, 1)
-    layout.addWidget(label8, 7, 0)
-    layout.addWidget(textBox8, 7, 1)
-    layout.addWidget(label9, 8, 0)
-    layout.addWidget(textBox9, 8, 1)
-    layout.addWidget(label10, 9, 0)
-    layout.addWidget(textBox10, 9, 1)
-    layout.addWidget(label11, 10, 0)
-    layout.addWidget(textBox11, 10, 1)
-
-    layout.addWidget(button1, 11, 0, 1, 2)
-    layout.addWidget(bigEditor, 12, 0, 1, 2)
-
-    window.setLayout(layout)
+    window.setLayout(set_layout_window())
     window.show()
 
 
